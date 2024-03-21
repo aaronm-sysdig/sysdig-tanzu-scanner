ae70e5d600c12cda87f195d1d7f55cf6fab691de
Aaron Miles <aaron.miles@sysdig.com>
2024-03-21 19:03:05 +1100

    Trying to fix versioning

--- sysdig-tanzu-scanner.go
+++ sysdig-tanzu-scanner.go
@@ -1586,8 +1586,8 @@ func main() {
 	done := make(chan bool, 1)
 	// Register the signals you want to catch
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	var VERSION = "1.0.0.0"
-	log.Printf("main:: Sysdig-Tanzu-Scanner v%s-BW Enter()", VERSION)
+	var version = "1.2.5"
+	log.Printf("main:: Sysdig-Tanzu-Scanner v%s-BW Enter()", version)
 
 	// Parse yaml config file
 	yamlConfig, err := parseConfigFile()
